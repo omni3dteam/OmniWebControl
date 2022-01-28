00c362a73fa15e41444cfc2386e0225b04e6dd41
MSzOmni3D <msz@omni3d.net>
2022-01-28 13:34:10 +0100

    version bump

--- src/components/panels/SettingsAboutPanel.vue
+++ src/components/panels/SettingsAboutPanel.vue
@@ -11,7 +11,7 @@
 			</template>
 		</v-card-text>
 		<v-card-text class="pt-0">
-			<a href="https://github.com/omni3dteam/OmniWebControl" target="_blank">Omni Web Control 2.5.2</a>
+			<a href="https://github.com/omni3dteam/OmniWebControl" target="_blank">Omni Web Control 2.5.2_dualBed</a>
 		</v-card-text>
 
 		<v-card-text class="pt-0">
